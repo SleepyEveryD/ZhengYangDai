f95410382071f1bfa50206a397791633c71959f5
jia-yi-Z <161482923+jia-yi-Z@users.noreply.github.com>
2026-01-14 19:02:22 +0100

    pathReport

--- Project/backend/src/app.module.ts
+++ Project/backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PathReportModule } from './path-report/path-report.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, PathReportModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ Project/backend/src/prisma/prisma.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  private pool: Pool;
+
+  constructor() {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+    // 用 DATABASE_URL 建立 pg 连接池
+    const connectionString = process.env.DATABASE_URL;
+    if (!connectionString) {
+      throw new Error('DATABASE_URL is missing');
+    }
+    
+    const pool = new Pool({
+        connectionString,
+        ssl: true,
+    });
+
+    const adapter = new PrismaPg(pool);
+
+    super({ adapter });
+
+    this.pool = pool;
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+    await this.pool.end();
+  }
+}
