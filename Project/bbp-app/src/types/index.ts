3b9834f72a8a25cfd5da2f5d258535ce708b1b2b
dairunjie <runjie.dai@mail.polimi.it>
2026-01-15 20:14:11 +0100

    stashes

---
+++ Project/bbp-app/src/types/index.ts
@@ -0,0 +1,4 @@
+export * from './issue';
+export * from './ride';
+export * from './route';
+export * from './user';
--- Project/bbp-app/src/types/ride.ts
+++ Project/bbp-app/src/types/ride.ts
@@ -1,12 +1,12 @@
-import type { Issue } from "./issue";
+import type { Issue } from './issue';
 
 export type Ride = {
   id: string;
   date: string;
-  distance: number;
-  duration: number;
-  avgSpeed: number;
-  maxSpeed: number;
+  distance: number;      // km
+  duration: number;      // seconds
+  avgSpeed: number;      // km/h
+  maxSpeed?: number;     // km/h
   path: [number, number][];
   issues: Issue[];
 };
