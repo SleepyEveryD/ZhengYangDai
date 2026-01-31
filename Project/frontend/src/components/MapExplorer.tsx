99efb84d0e1a590a525d2335ccb79b328f9b101f
Jiaxin <jiaxin.yang@mail.polimi.it>
2026-01-31 16:41:37 +0100

    remove gray

--- Project/frontend/src/components/MapExplorer.tsx
+++ Project/frontend/src/components/MapExplorer.tsx
@@ -82,7 +82,7 @@ export default function MapExplorer({ user }: MapExplorerProps) {
 
 
   return (
-    <div className="h-[100dvh] overflow-hidden flex flex-col bg-gray-50">
+    <div className="h-[100dvh] overflow-hidden flex flex-col ">
 
       {/* Top Bar */}
       <div className="absolute top-0 left-0 right-0 z-10 p-4">
